8610df3fc411fadd172401f5a437bffa56b0d614
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-10 15:12:43 +0900

    refactored

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,6 +18,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @review = Review.new
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post 'restaurants', to: 'restaurants#create', as: :restaurants
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-
-  resources :restaurants do
-    resources :reviews, only: [:new, :create]
+  resources :restaurants, only: %i[new create index show] do
+    resources :reviews
   end
+
+  post 'restaurant/:id', to: 'reviews#create'
 end
